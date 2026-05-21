c6a376662a4a0ff71aa6a2fa006dbad6079785d8
JM Bell <johnmichaelbell@gmail.com>
2026-05-21 02:25:53 -0400

    updating neovim plugin

--- modules/features/neovim.nix
+++ modules/features/neovim.nix
@@ -1,125 +1,122 @@
 # modules/features/neovim.nix
-{ inputs, ... }:
-{
-  flake.nixosModules.neovim =
-    { ... }:
-    {
-      imports = [ inputs.nvf.nixosModules.default ];
+{inputs, ...}: {
+  flake.nixosModules.neovim = {...}: {
+    imports = [inputs.nvf.nixosModules.default];
 
-      programs.nvf.enable = true;
-      programs.nvf.settings.vim = {
-        viAlias = true;
-        vimAlias = true;
+    programs.nvf.enable = true;
+    programs.nvf.settings.vim = {
+      viAlias = true;
+      vimAlias = true;
 
-        opts.expandtab = true;
+      opts.expandtab = true;
 
-        spellcheck.enable = true;
+      spellcheck.enable = true;
 
-        lsp = {
-          enable = true;
-          formatOnSave = true;
-          lspkind.enable = false;
-          lightbulb.enable = true;
-          lspsaga.enable = false;
-          trouble.enable = true;
-          lspSignature.enable = true;
-        };
-
-        debugger.nvim-dap = {
-          enable = true;
-          ui.enable = true;
-        };
-
-        languages = {
-          enableFormat = true;
-          enableTreesitter = true;
-          enableExtraDiagnostics = true;
-          nix.enable = true;
-          markdown.enable = true;
-        };
-
-        visuals = {
-          nvim-web-devicons.enable = true;
-          nvim-cursorline.enable = true;
-          cinnamon-nvim.enable = true;
-          fidget-nvim.enable = true;
-          highlight-undo.enable = true;
-          blink-indent.enable = true;
-          indent-blankline.enable = true;
-        };
-
-        statusline.lualine = {
-          enable = true;
-          theme = "auto";
-        };
-
-        theme = {
-          enable = true;
-          name = "catppuccin";
-          style = "mocha";
-          transparent = false;
-        };
-
-        autopairs.nvim-autopairs.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        snippets.luasnip.enable = true;
-
-        filetree.neo-tree.enable = true;
-        tabline.nvimBufferline.enable = true;
-        treesitter.context.enable = true;
-
-        binds = {
-          whichKey.enable = true;
-          cheatsheet.enable = true;
-        };
-
-        telescope.enable = true;
-
-        git = {
-          enable = true;
-          gitsigns.enable = true;
-          gitsigns.codeActions.enable = false;
-        };
-
-        notify.nvim-notify.enable = true;
-
-        utility = {
-          diffview-nvim.enable = true;
-          motion = {
-            hop.enable = true;
-            leap.enable = true;
-          };
-          oil-nvim.enable = true;
-        };
-
-        notes.todo-comments.enable = true;
-
-        terminal.toggleterm = {
-          enable = true;
-          lazygit.enable = true;
-        };
-
-        ui = {
-          borders.enable = true;
-          noice.enable = true;
-          colorizer.enable = true;
-          illuminate.enable = true;
-          smartcolumn = {
-            enable = true;
-            setupOpts.custom_colorcolumn = {
-              nix = "110";
-              ruby = "120";
-              java = "130";
-              go = [
-                "90"
-                "130"
-              ];
-            };
-          };
-          fastaction.enable = true;
-        };
-
-        comments.comment-nvim.enable = true;
+      lsp = {
+        enable = true;
+        formatOnSave = true;
+        lspkind.enable = false;
+        lightbulb.enable = true;
+        lspsaga.enable = false;
+        trouble.enable = true;
+        lspSignature.enable = true;
       };
+
+      debugger.nvim-dap = {
+        enable = true;
+        ui.enable = true;
+      };
+
+      languages = {
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
+        nix.enable = true;
+        markdown.enable = true;
+      };
+
+      visuals = {
+        nvim-web-devicons.enable = true;
+        nvim-cursorline.enable = true;
+        cinnamon-nvim.enable = true;
+        fidget-nvim.enable = true;
+        highlight-undo.enable = true;
+        blink-indent.enable = true;
+        indent-blankline.enable = true;
+      };
+
+      statusline.lualine = {
+        enable = true;
+        theme = "auto";
+      };
+
+      theme = {
+        enable = true;
+        name = "catppuccin";
+        style = "mocha";
+        transparent = false;
+      };
+
+      autopairs.nvim-autopairs.enable = true;
+      autocomplete.nvim-cmp.enable = true;
+      snippets.luasnip.enable = true;
+
+      filetree.neo-tree.enable = true;
+      tabline.nvimBufferline.enable = true;
+      treesitter.context.enable = true;
+
+      binds = {
+        whichKey.enable = true;
+        cheatsheet.enable = true;
+      };
+
+      telescope.enable = true;
+
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+        gitsigns.codeActions.enable = false;
+      };
+
+      notify.nvim-notify.enable = true;
+
+      utility = {
+        diffview-nvim.enable = true;
+        motion = {
+          hop.enable = true;
+          leap.enable = true;
+        };
+        oil-nvim.enable = true;
+      };
+
+      notes.todo-comments.enable = true;
+
+      terminal.toggleterm = {
+        enable = true;
+        lazygit.enable = true;
+      };
+
+      ui = {
+        borders.enable = true;
+        noice.enable = true;
+        colorizer.enable = true;
+        illuminate.enable = true;
+        smartcolumn = {
+          enable = true;
+          setupOpts.custom_colorcolumn = {
+            nix = "110";
+            ruby = "120";
+            java = "130";
+            go = [
+              "90"
+              "130"
+            ];
+          };
+        };
+        fastaction.enable = true;
+      };
+
+      comments.comment-nvim.enable = true;
     };
+  };
 }
